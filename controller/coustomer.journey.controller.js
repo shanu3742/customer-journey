3fbf1892b86afaf485db5b6e33f0f882463b7aaa
kumar <krksingh.99@gmail.com>
2024-11-10 14:35:49 +0530

    selectBox added

--- controller/coustomer.journey.controller.js
+++ controller/coustomer.journey.controller.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const coustomerData = require('../model/coustomer.journey.model');
 
 const onIndexRedirection = (req,res) => {
     console.log('wait to redirect')
@@ -9,8 +10,11 @@ const getCustomerJourney = (req,res) => {
     // Middleware to serve static files from the "public" directory
     res.status(200).sendFile(path.join(__dirname, '..', 'index.html'))
 }
+const getCustomerJourneyData = (req,res) => {
+    res.status(200).send(coustomerData)
+}
 
 const appController = (req, res) => {
     res.status(200).sendFile(path.join(__dirname, '..', 'app' ,'app.js'))
 }
-module.exports = {onIndexRedirection, getCustomerJourney,appController}
+module.exports = {onIndexRedirection, getCustomerJourney,getCustomerJourneyData,appController}
--- router/coustomer.journey.router.js
+++ router/coustomer.journey.router.js
@@ -1,4 +1,4 @@
-const { onIndexRedirection,getCustomerJourney, appController } = require('../controller/coustomer.journey.controller')
+const { onIndexRedirection,getCustomerJourney, appController, getCustomerJourneyData } = require('../controller/coustomer.journey.controller')
 const path = require('path');
 const express = require('express');
 const router = express.Router();
@@ -7,7 +7,8 @@ const router = express.Router();
 router.use(express.static(path.join(__dirname, 'public')));
 router.get('/',onIndexRedirection)
 router.get('/coustomer/journey/getJourney',getCustomerJourney)
+router.get('/coustomer/journey/getData',getCustomerJourneyData)
 router.get('/coustomer/journey/app',appController)
 
-    module.exports= router;
+module.exports= router;
 
