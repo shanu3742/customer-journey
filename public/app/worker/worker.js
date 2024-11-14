7eae0de6ed3a22c2ffce9947513ce76d8e34e590
kumar shanu <kumar.shanu@stockedge.com>
2024-11-14 19:18:51 +0530

    worker code updated

--- public/app/worker/worker.js
+++ public/app/worker/worker.js
@@ -2,42 +2,57 @@ importScripts('https://d3js.org/d3.v6.js');
 
 // Declare simulation outside the onmessage handler
 let simulation;
+let nodes=[];
+let links= [];
 
 self.onmessage = (event) => {
-    const { nodes, links, width, height, type } = event.data;
 
-    if (type === 'play') {
-        // Initialize the simulation only if it hasn't been created yet
-        if (!simulation) {
-            simulation = d3.forceSimulation(nodes)
-                .force("link", d3.forceLink()
-                    .id(d => d.id)
-                    .links(links)
-                )
-                .force("charge", d3.forceManyBody().strength(-15))
-                .force("center", d3.forceCenter(width / 2, height / 2))
-                .on("tick", () => {
-                    self.postMessage({ nodes: nodes, links: links, type: 'tick' });
-                })
-                .on("end", () => {
-                    self.postMessage({ nodes: nodes, links: links, type: 'end' });
-                });
-        } else {
-            // Restart the simulation if it's already initialized
-            simulation.alphaTarget(0.3).restart()
-        }
+    if (event.data.type ==='init') {
+            const { width, height } = event.data;
+            //get the node data when simulation run first time
+            //and stor it in global scope of worker file 
+            if(!nodes.length && !links.length){
+                nodes = event.data.nodes;
+                links = event.data.links;
+            }
+            // Initialize the simulation only if it hasn't been created yet
+            //and also store the simulation in global scope of worker file 
+            if (!simulation) {
+                simulation = d3.forceSimulation(nodes)
+                    .force("link", d3.forceLink()
+                        .id(d => d.id)
+                        .links(links)
+                    )
+                    .force("charge", d3.forceManyBody().strength(-15))
+                    .force("center", d3.forceCenter(width / 2, height / 2))
+                    .on("tick", () => {
+                        self.postMessage({ nodes: nodes, links: links, type: 'tick' });
+                    })
+                    .on("end", () => {
+                        self.postMessage({ nodes: nodes, links: links, type: 'end' });
+                    });
+            } else {
+                // Restart the simulation if it's already initialized
+                simulation.alphaTarget(0.3).restart()
+            }
+    }else{
+            if(event.data.type ==='dragStart'){
+                //get the drag cordinate and update the draging node for the fix cordinate 
+                const { nodeIndex, x, y } = event.data;
+                nodes[nodeIndex].fx = x;
+                nodes[nodeIndex].fy = y;
+
+                // restart the simulation  
+                simulation.alphaTarget(0.3).restart();
+            }
+            if(event.data.type === 'dragEnd'){
+                const { nodeIndex } = event.data;
+                //relase the node from fixed cordinate 
+                nodes[nodeIndex].fx = null;
+                nodes[nodeIndex].fy = null;
+                simulation.alphaTarget(0)
+            }
     }
 
-    if (type === 'resume') {
-        if (simulation) {
-            simulation.alphaTarget(0.3).restart();
-        }
-    }
-
-    if (type === 'stop') {
-        if (simulation) {
-            console.log(simulation); // Now this should print the simulation object
-            simulation.alphaTarget(0); // Stop the simulation
-        }
-    }
+    
 };
