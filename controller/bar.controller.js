5d496b63e25a64886e7c8ced5e1e4f2cbb441546
kumar shanu <kumar.shanu@stockedge.com>
2024-11-15 19:17:42 +0530

    bar page ctrl added

--- controller/bar.controller.js
+++ controller/bar.controller.js
@@ -4,4 +4,7 @@ const getbar = (req,res) => {
     // Middleware to serve static files from the "public" directory
     res.status(200).sendFile(path.join(__dirname, '..','public','page','html' ,'bar.html'))
 }
-module.exports = {getbar}
+const getbarPageCtr = (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, '..', 'public','app','page-controller' ,'bar.page.controller.js'))
+}
+module.exports = {getbar,getbarPageCtr}
--- router/bar.router.js
+++ router/bar.router.js
@@ -1,7 +1,8 @@
 
 const path = require('path');
 const express = require('express');
-const { getbar } = require('../controller/bar.controller');
+const { getbar, getbarPageCtr } = require('../controller/bar.controller');
 const router = express.Router();
 router.get('/coustomer/journey/bar',getbar);
+router.get('/coustomer/journey/getbarPageCtr',getbarPageCtr)
 module.exports= router;
