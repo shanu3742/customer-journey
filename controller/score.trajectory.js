22f5952d809c278120ab7b7c8c5a2e51e557d6e3
kumar shanu <kumar.shanu@stockedge.com>
2024-11-19 17:40:29 +0530

    score trajectory graph added

--- controller/score.trajectory.js
+++ controller/score.trajectory.js
@@ -1,7 +1,12 @@
 const path = require('path');
+const scoreTrajectoryData = require('../model/score.trajectory.model');
 const getScoreTrajectory = (req,res) => {
     // Middleware to serve static files from the "public" directory
     res.status(200).sendFile(path.join(__dirname, '..','public','page','html' ,'score-trajectory-graph.html'))
+}
+
+const getScoreTrajectoryData = (req,res) => {
+    res.status(200).send(scoreTrajectoryData)
 }
 
 
@@ -9,4 +14,4 @@ const getScoreTrajectoryPageCtr = (req, res) => {
     res.status(200).sendFile(path.join(__dirname, '..', 'public','app','page-controller' ,'score.trajectory.page.controller.js'))
 }
 
-module.exports = {getScoreTrajectory,getScoreTrajectoryPageCtr}
+module.exports = {getScoreTrajectory,getScoreTrajectoryPageCtr,getScoreTrajectoryData}
--- router/score.trajectory.router.js
+++ router/score.trajectory.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { getScoreTrajectory, getScoreTrajectoryPageCtr } = require('../controller/score.trajectory');
+const { getScoreTrajectory, getScoreTrajectoryPageCtr, getScoreTrajectoryData } = require('../controller/score.trajectory');
 const router = express.Router();
 router.get('/coustomer/journey/scoreTrajectory',getScoreTrajectory);
+router.get('/coustomer/journey/getScoreTrajectoryData',getScoreTrajectoryData)
 router.get('/coustomer/journey/getScoreTrajectoryPageCtr',getScoreTrajectoryPageCtr)
 module.exports= router;
